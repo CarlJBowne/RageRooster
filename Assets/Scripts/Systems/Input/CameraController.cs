355c5b0659431d1c88902d371adf814eb5907928
RobbyGman <ro305529@ucf.edu>
2024-09-30 12:20:23 -0400

    Failed to add multi-jump

--- Assets/Scripts/Systems/Input/CameraController.cs
+++ Assets/Scripts/Systems/Input/CameraController.cs
@@ -8,7 +8,7 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         Vector2 lookInput = Input.Camera;
-        freeLookCamera.m_XAxis.Value += lookInput.x * Time.deltaTime * 300f; // Adjust sensitivity as needed
-        freeLookCamera.m_YAxis.Value += lookInput.y * Time.deltaTime * 2f;   // Adjust sensitivity as needed
+        freeLookCamera.m_XAxis.Value += lookInput.x * Time.deltaTime * 300f;
+        freeLookCamera.m_YAxis.Value += lookInput.y * Time.deltaTime * 2f;
     }
 }
