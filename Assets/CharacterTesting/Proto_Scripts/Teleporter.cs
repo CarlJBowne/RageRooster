dd846bd905302cdc874870b29e29a9add567e0a9
RobbyGman <ro305529@ucf.edu>
2024-10-05 04:14:57 -0400

    MouseLock&Prototype teleport
    Added a lock so the mouse stays within the screen and becomes invisible. I also add a player teleporter in the test chamber scene to better test the parachute

---
+++ Assets/CharacterTesting/Proto_Scripts/Teleporter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField] private Transform targetLocation;
+    [SerializeField] private string targetTag = "Player";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter called with: " + other.name); // Debug log
+
+        if (other.CompareTag(targetTag))
+        {
+            Debug.Log("Teleporting " + other.name + " to " + targetLocation.position); // Debug log
+
+            CharacterController characterController = other.GetComponent<CharacterController>();
+            if (characterController != null)
+            {
+                Debug.Log("Disabling CharacterController for: " + other.name); // Debug log
+                characterController.enabled = false; // Disable CharacterController
+                other.transform.position = targetLocation.position; // Teleport
+                characterController.enabled = true; // Re-enable CharacterController
+                Debug.Log("Re-enabled CharacterController for: " + other.name); // Debug log
+            }
+            else
+            {
+                other.transform.position = targetLocation.position; // Teleport if no CharacterController
+            }
+        }
+    }
+}
