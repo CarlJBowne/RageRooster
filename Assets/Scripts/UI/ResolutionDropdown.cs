02a8be1b9ae0145a843c75cd3a96acde9087617d
RobbyGman <ro305529@ucf.edu>
2024-11-03 21:54:33 -0500

    Update ResolutionDropdown.cs

--- Assets/Scripts/UI/ResolutionDropdown.cs
+++ Assets/Scripts/UI/ResolutionDropdown.cs
@@ -8,10 +8,22 @@ public class ResolutionDropdown : MonoBehaviour
     public TMP_Dropdown resolutionDropdown;
 
     private List<Resolution> resolutions = new List<Resolution>();
+    private List<Resolution> predefinedResolutions = new List<Resolution>
+    {
+        new Resolution { width = 3840, height = 2160 }, 
+        new Resolution { width = 2560, height = 1440 }, 
+        new Resolution { width = 2048, height = 1080 },
+        new Resolution { width = 1920, height = 1080 },
+        new Resolution { width = 1600, height = 900 },
+        new Resolution { width = 1366, height = 768 },
+        new Resolution { width = 1280, height = 720 },
+        new Resolution { width = 1024, height = 768 },
+        new Resolution { width = 800, height = 600 }
+    };
 
     void Start()
     {
-        resolutions.AddRange(Screen.resolutions);
+        resolutions.AddRange(predefinedResolutions);
         resolutionDropdown.ClearOptions();
 
         List<string> options = new List<string>();
@@ -40,5 +52,8 @@ public class ResolutionDropdown : MonoBehaviour
     {
         Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+
+        // Debug log to verify resolution change
+        Debug.Log("Resolution set to: " + resolution.width + " x " + resolution.height);
     }
 }
