b5632ff102a5fd660fabffa2508e22f0b81a2897
RobbyGman <ro305529@ucf.edu>
2024-11-12 16:30:14 -0500

    updated brightness control
    looks in the scene for the brightness slider and overlay when the scene is loaded

--- Assets/Scripts/UI/BrightnessController.cs
+++ Assets/Scripts/UI/BrightnessController.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class BrightnessController : MonoBehaviour
 {
     public Slider brightnessSlider;
     public Image brightnessOverlay;
 
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
     void OnEnable()
     {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        brightnessSlider = FindInactiveSlider();
+        brightnessOverlay = FindInactiveImage();
         float savedBrightness = PlayerPrefs.GetFloat("Brightness", 0.5f);
         brightnessSlider.value = savedBrightness;
         brightnessSlider.onValueChanged.AddListener(OnBrightnessChanged);
@@ -18,7 +28,21 @@ public class BrightnessController : MonoBehaviour
 
     void OnDisable()
     {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
         brightnessSlider.onValueChanged.RemoveListener(OnBrightnessChanged);
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        brightnessSlider = FindInactiveSlider();
+        brightnessOverlay = FindInactiveImage();
+        if (brightnessSlider != null && brightnessOverlay != null)
+        {
+            float savedBrightness = PlayerPrefs.GetFloat("Brightness", 0.5f);
+            brightnessSlider.value = savedBrightness;
+            brightnessSlider.onValueChanged.AddListener(OnBrightnessChanged);
+            InitializeBrightness();
+        }
     }
 
     void OnBrightnessChanged(float value)
@@ -41,5 +65,31 @@ public class BrightnessController : MonoBehaviour
         brightnessOverlay.color = color;
 
         Debug.Log($"Brightness value: {value}, Alpha: {color.a}");
+    }
+
+    Slider FindInactiveSlider()
+    {
+        Slider[] sliders = Resources.FindObjectsOfTypeAll<Slider>();
+        foreach (Slider slider in sliders)
+        {
+            if (slider.gameObject.name == "BrightnessSlider")
+            {
+                return slider;
+            }
+        }
+        return null;
+    }
+
+    Image FindInactiveImage()
+    {
+        Image[] images = Resources.FindObjectsOfTypeAll<Image>();
+        foreach (Image image in images)
+        {
+            if (image.gameObject.name == "BrightnessOverlay")
+            {
+                return image;
+            }
+        }
+        return null;
     }
 }
