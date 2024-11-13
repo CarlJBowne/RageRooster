de9e5bf2a43a25d9993f0471ae99c4b23ed7bfc8
CBowneUCF <156275809+CBowneUCF@users.noreply.github.com>
2024-11-12 23:55:30 -0500

    Singleton Overhaul + GlobalSingleton Implementation

--- Assets/Testing/TestSingleton.cs
+++ Assets/Testing/TestSingleton.cs
@@ -1,21 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class TestSingleton : Singleton<TestSingleton>
 {
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
-    private static void AAAA()
-    {
-        prefab = FindFirstObjectByType<TestSingleton>(FindObjectsInactive.Include).gameObject;
-        Instantiate(prefab);
-    }
-    public static GameObject prefab;
+    private static void Boot() => SetInfo(InitSavedPrefab, true, true);
 
 
     protected override void OnAwake()
     {
         Debug.Log("Success");
     }
+
+    public string a = "AAA";
 }
