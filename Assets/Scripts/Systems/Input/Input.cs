d98ab1df403f73c5f09801e83a5d7e7205d0c167
CBowneUCF <156275809+CBowneUCF@users.noreply.github.com>
2024-11-21 23:19:58 -0500

    Controls Edit

--- Assets/Scripts/Systems/Input/Input.cs
+++ Assets/Scripts/Systems/Input/Input.cs
@@ -19,23 +19,23 @@ public class Input : Staticon<Input>
 	public static Vector2 Camera => Get().asset.Gameplay.Camera.ReadValue<Vector2>();
 	public static Button Jump => Get().asset.Gameplay.Jump;
 	public static Button Attack => Get().asset.Gameplay.Attack;
-	public static Button Block => Get().asset.Gameplay.Block;
+	public static Button Parry => Get().asset.Gameplay.Parry;
 	public static Button Grab => Get().asset.Gameplay.Grab;
 	public static Button ShootMode => Get().asset.Gameplay.ShootMode;
 	public static Button Shoot => Get().asset.Gameplay.Shoot;
 	public static Button Charge => Get().asset.Gameplay.Charge;
-	public static Button Sonic => Get().asset.Gameplay.Sonic;
+	public static Button Interact => Get().asset.Gameplay.Interact;
 
 	public Vector2 movement => asset.Gameplay.Movement.ReadValue<Vector2>();
 	public Vector2 camera => asset.Gameplay.Camera.ReadValue<Vector2>();
 	public Button jump => asset.Gameplay.Jump;
 	public Button attack => asset.Gameplay.Attack;
-	public Button block => asset.Gameplay.Block;
+	public Button parry => asset.Gameplay.Parry;
 	public Button grab => asset.Gameplay.Grab;
 	public Button shootMode => asset.Gameplay.ShootMode;
 	public Button shoot => asset.Gameplay.Shoot;
 	public Button charge => asset.Gameplay.Charge;
-	public Button sonic => asset.Gameplay.Sonic;
+	public Button interact => asset.Gameplay.Interact;
 
 
 }
