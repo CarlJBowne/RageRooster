7e3038cace473e94592d88e5a0485c72294f5b88
JTShocks <jacobdreyer2002@gmail.com>
2025-02-22 11:01:54 -0500

    Update TMP_Animated.cs
    Incorporated the rest of the code present in the repo. Thanks to Andre Cardoso for providing a great resource to take inspiration from

--- Assets/Scripts/UI/Dialog_System/TMP_Animated.cs
+++ Assets/Scripts/UI/Dialog_System/TMP_Animated.cs
@@ -15,6 +15,13 @@ namespace TMPro
         // Jacob Dreyer
         // Creating a custom text element that can be animated and has a controlled text speed
 
+        /*
+        Original Authors
+            André Cardoso - Github
+        License
+            This project is licensed under the MIT License - see the LICENSE.md file for details
+        */
+
         [SerializeField] private float textSpeed = 10; //TODO:  Replace with a reference to a global setting for players to control default speed
         public ActionEvent onAction;
         public TextRevealEvent onTextReveal;
@@ -28,6 +35,77 @@ namespace TMPro
             string[] subTexts = newText.Split('<', '>');
 
             
+            // textmeshpro still needs to parse its built-in tags, so we only include noncustom tags
+            string displayText = "";
+            for (int i = 0; i < subTexts.Length; i++)
+            {
+                if (i % 2 == 0)
+                    displayText += subTexts[i];
+                else if (!isCustomTag(subTexts[i].Replace(" ", "")))
+                    displayText += $"<{subTexts[i]}>";
+            }
+
+            // send that string to textmeshpro and hide all of it, then start reading
+            text = displayText;
+            maxVisibleCharacters = 0;
+            StartCoroutine(Read());
+
+            IEnumerator Read()
+            {
+                int subCounter = 0;
+                int visibleCounter = 0;
+                while (subCounter < subTexts.Length)
+                {
+                    // if 
+                    if (subCounter % 2 == 1)
+                    {
+                        yield return EvaluateTag(subTexts[subCounter].Replace(" ", ""));
+                    }
+                    else
+                    {
+                        while (visibleCounter < subTexts[subCounter].Length)
+                        {
+                            onTextReveal.Invoke(subTexts[subCounter][visibleCounter]);
+                            visibleCounter++;
+                            maxVisibleCharacters++;
+                            yield return new WaitForSeconds(1f / textSpeed);
+                        }
+                        visibleCounter = 0;
+                    }
+                    subCounter++;
+                }
+                yield return null;
+                
+                WaitForSeconds EvaluateTag(string tag)
+                {
+                    if (tag.Length > 0)
+                    {
+                        if (tag.StartsWith("speed="))
+                        {
+                            textSpeed = float.Parse(tag.Split('=')[1]);
+                        }
+                        else if (tag.StartsWith("pause="))
+                        {
+                            return new WaitForSeconds(float.Parse(tag.Split('=')[1]));
+                        }
+                        else if (tag.StartsWith("action="))
+                        {
+                            onAction.Invoke(tag.Split('=')[1]);
+                        }
+                    }
+                    return null;
+                }
+                onDialogueFinish.Invoke();
+            }
+
+        }
+
+        
+
+        // check to see if a tag is our own
+        bool isCustomTag(string tag)
+        {
+            return tag.StartsWith("speed=") || tag.StartsWith("pause=") || tag.StartsWith("emotion=") || tag.StartsWith("action");
         }
 
     }
