1b5a48e705c899b65e5320132b3793d854b85a88
reireizo <nathan.irwin2021@gmail.com>
2025-03-06 15:23:50 -0500

    Begin Bomber Enemy

---
+++ Assets/Scripts/Entities/Exploder.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exploder : MonoBehaviour
+{
+    public void Explode()
+    {
+        Debug.Log("Boom!");
+    }
+}
