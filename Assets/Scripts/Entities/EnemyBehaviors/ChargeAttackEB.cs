a33f2971f9eee278fc9bff8f1ed7f9b858de6eb6
reireizo <nathan.irwin2021@gmail.com>
2025-03-12 17:45:40 -0400

    Current Charge Attack EB Progress

---
+++ Assets/Scripts/Entities/EnemyBehaviors/ChargeAttackEB.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SLS.StateMachineV3;
+using UnityEngine.AI;
+using UnityEngine.Rendering;
+
+public class ChargeAttackEB : StateBehavior
+{
+    Rigidbody rb;
+    Vector3 direction;
+    public State vulnerableState;
+    float wallCheckDistance = 2f;
+    public override void OnEnter(State prev, bool isFinal)
+    {
+        rb = GetComponentFromMachine<Rigidbody>();
+        direction = (Gameplay.Player.transform.position - rb.transform.position).normalized;
+        rb.transform.rotation = Quaternion.LookRotation(direction);
+    }
+
+    public override void OnFixedUpdate()
+    {
+        RaycastHit hit;
+        if (rb.DirectionCast(direction, wallCheckDistance, 1f, out hit))
+        {
+            TransitionTo(vulnerableState);
+        }
+    }
+}
