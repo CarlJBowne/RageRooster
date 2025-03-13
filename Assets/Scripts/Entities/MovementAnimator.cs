7015f6c255b39671024ed7c9856266aa31ca2683
CBowneUCF <156275809+CBowneUCF@users.noreply.github.com>
2025-03-13 15:21:46 -0400

    Boss1 Anim placeholder setup

--- Assets/Scripts/Entities/MovementAnimator.cs
+++ Assets/Scripts/Entities/MovementAnimator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MovementAnimator : MonoBehaviour
 {
+    [Header("Only touch if animating.")]
     public float influence = 0;
     public Vector3 relativeVelocity;
     public float angularVelocity;
@@ -11,22 +12,31 @@ public class MovementAnimator : MonoBehaviour
 
     private Rigidbody rb;
     private Transform target;
-    private void Awake() => TryGetComponent(out rb);
+    private void Awake()
+    {
+        TryGetComponent(out rb);
+        target = Gameplay.Player.transform;
+        influence = 0;
+        relativeVelocity = Vector3.zero;
+        angularVelocity = 0;
+        turnToVelocity = 0;
+    }
 
     private void FixedUpdate()
     {
         if(influence > 0)
         {
             Vector3 trueRelativeVelocity = transform.TransformDirection(relativeVelocity);
-            rb.velocity = Vector3.MoveTowards(rb.velocity, trueRelativeVelocity, (trueRelativeVelocity - rb.velocity).magnitude * influence);
+            rb.velocity = Vector3.MoveTowards(rb.velocity, trueRelativeVelocity, influence * (trueRelativeVelocity - rb.velocity).magnitude);
 
             float tempAngularVelocity = rb.angularVelocity.y;
 
-            tempAngularVelocity = tempAngularVelocity.MoveTowards((angularVelocity - tempAngularVelocity) * influence, angularVelocity);
+            tempAngularVelocity = tempAngularVelocity.MoveTowards(influence * (angularVelocity - tempAngularVelocity), angularVelocity);
             rb.angularVelocity = new(0, tempAngularVelocity, 0);
 
-            Vector3 targetDirection = (target.position - transform.position).XZ();
-            transform.eulerAngles = Vector3.RotateTowards(transform.forward, targetDirection, (targetDirection - transform.position).magnitude * influence, 0);
+            Vector3 targetDirection = target.position - transform.position;
+            targetDirection.y = 0;
+            transform.eulerAngles = Vector3.RotateTowards(transform.forward, targetDirection, influence * turnToVelocity * 2 * Mathf.PI, 0);
         }
     }
 }
