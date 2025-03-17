4c6e73b7eecc51e608382fe211b35e22f369dc0a
CBowneUCF <156275809+CBowneUCF@users.noreply.github.com>
2025-03-16 23:45:43 -0400

    Boss 1 In Context application

--- Assets/Scripts/Entities/SavePoint.cs
+++ Assets/Scripts/Entities/SavePoint.cs
@@ -24,8 +24,8 @@ public class SavePoint : MonoBehaviour, IInteractable
     [Button]
     private void BeginFromHere()
     {
-        GetComponentInParent<ZoneRoot>().SetSpawn(this);
         UnityEditor.EditorApplication.isPlaying = true;
+        GetComponentInParent<ZoneRoot>().SetSpawn(this);
     }
 #endif
 
