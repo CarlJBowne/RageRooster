f69e4f98f21be0f0f3037d69a3541211576785ad
CBowneUCF <156275809+CBowneUCF@users.noreply.github.com>
2025-03-23 14:51:53 -0400

    Boss Head 2 Attack 1

---
+++ Assets/Scripts/Entities/EnemyBehaviors/BossBehaviors/Boss2Attacks/B2AC_Peck.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+using THIS = B2AC_Peck;
+
+public class B2AC_Peck : RigConstraint<THIS.Job, THIS.Data, THIS.Binder>
+{
+    public float followPlayerRate;
+    public float yPosition = 8.19f;
+
+    public struct Job : IWeightedAnimationJob
+    {
+        public ReadWriteTransformHandle constrained;
+        public ReadOnlyTransformHandle source;
+        public FloatProperty jobWeight { get; set; }
+
+        public FloatProperty followPlayerRate;
+        public FloatProperty yPosition;
+
+
+        public void ProcessRootMotion(UnityEngine.Animations.AnimationStream stream) { }
+        public void ProcessAnimation(UnityEngine.Animations.AnimationStream stream)
+        {
+            if (!stream.isValid) return; // Ensure valid data before proceeding
+
+            float followPlayerRate = this.followPlayerRate.Get(stream);
+            float yPosition = this.yPosition.Get(stream);
+
+            Vector3 newPosition = constrained.GetPosition(stream);
+
+            if (followPlayerRate > 0)
+                try
+                {
+                    newPosition = Vector3.MoveTowards(constrained.GetPosition(stream).XZ(), source.GetPosition(stream).XZ(), followPlayerRate * Time.deltaTime);
+                }
+                catch
+                {
+                    newPosition = Vector3.MoveTowards(constrained.GetPosition(stream).XZ(), source.GetPosition(stream).XZ(), followPlayerRate);
+                }
+
+            newPosition.y = yPosition;
+
+            constrained.SetPosition(stream, Vector3.Lerp(constrained.GetPosition(stream), newPosition, jobWeight.Get(stream)));
+        }
+    }
+
+    [System.Serializable]
+    public struct Data : IAnimationJobData
+    {
+        public Transform constrainedObject;
+        [SyncSceneToStream] public Transform sourceObject;
+
+        public bool IsValid() => !(constrainedObject == null || sourceObject == null);
+        public void SetDefaultValues()
+        {
+            constrainedObject = null;
+            sourceObject = null;
+        }
+    }
+
+    public class Binder : AnimationJobBinder<Job, Data>
+    {
+        public override Job Create(Animator animator, ref Data data, Component component)
+            => new()
+            {
+                constrained = ReadWriteTransformHandle.Bind(animator, data.constrainedObject),
+                source = ReadOnlyTransformHandle.Bind(animator, data.sourceObject),
+                followPlayerRate = FloatProperty.Bind(animator, component, nameof(B2AC_Peck.followPlayerRate)),
+                yPosition = FloatProperty.Bind(animator, component, nameof(B2AC_Peck.yPosition))
+            };
+
+        public override void Destroy(Job job) { }
+    }
+}
+
