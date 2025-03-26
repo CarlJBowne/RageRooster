1c76cd4e79c3ccdf665369a9dfd13e4e355c707d
CBowneUCF <156275809+CBowneUCF@users.noreply.github.com>
2025-03-26 01:56:57 -0400

    Final Boss Damage Systems Part 1

--- Assets/Scripts/Entities/EnemyBehaviors/BossBehaviors/Boss2CentralController.cs
+++ Assets/Scripts/Entities/EnemyBehaviors/BossBehaviors/Boss2CentralController.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss2CentralController : MonoBehaviour
+public class Boss2CentralController : Health
 {
     public Boss2HeadStateMachine Pecky;
     public Boss2HeadStateMachine Slasher;
     public Boss2HeadStateMachine Stumpy;
-    public Boss2Health health;
+    
 
     public UltEvents.UltEvent ResetBossEvent;
     public UltEvents.UltEvent FinishBossEvent;
 
     private void Start()
     {
-        Pecky.animator.Play("Idle_Pecky");
-        Slasher.animator.Play("Idle_Slasher");
-        Stumpy.animator.Play("Idle_Stumpy");
+        Pecky.GoToIdle();
+        Slasher.GoToIdle();
+        Stumpy.GoToIdle();
     }
 
     private void FixedUpdate()
@@ -27,5 +27,30 @@ public class Boss2CentralController : MonoBehaviour
     public void ResetBoss() => ResetBossEvent?.Invoke();
 
     public void FinishBoss() => FinishBossEvent?.Invoke();
+
+    protected override bool OverrideDamageable(Attack attack)
+    {
+        return attack.HasTag("FromPlayer") && attack.HasTag("OnWeakSpot");
+    }
+
+    public void CheckIfBothKnocked()
+    {
+        if(Pecky.currentState == Boss2HeadStateMachine.knockedState && Slasher.currentState == Boss2HeadStateMachine.knockedState)
+        {
+            Invoke(nameof(StartStumpyVulnerable), 5f);
+        }
+    }
+    private void StartStumpyVulnerable()
+    {
+        Stumpy.animator.CrossFade("Stumpy_Vulnerable", 0.2f);
+        Stumpy.currentState = "Vulnerable";
+    }
+
+    public void VulnerableReturn()
+    {
+        Pecky.GoToIdle();
+        Slasher.GoToIdle();
+        Stumpy.GoToIdle();
+    }
 
 }
--- Assets/Scripts/Entities/EnemyBehaviors/BossBehaviors/Boss2HeadStateMachine.cs
+++ Assets/Scripts/Entities/EnemyBehaviors/BossBehaviors/Boss2HeadStateMachine.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using EditorAttributes;
 using SLS.StateMachineV3;
 
-public class Boss2HeadStateMachine : StateMachine, IDamagable
+public class Boss2HeadStateMachine : MonoBehaviour, IDamagable
 {
     public FinalBossHead headID;
 
@@ -12,11 +12,10 @@ public class Boss2HeadStateMachine : StateMachine, IDamagable
     public int attackTimeLower;
     public int attackTimeHigher;
 
-    public State idleState;
-    public State guardingState;
-    public State knockedState;
-    public State attack1State;
-    public State attack2State;
+    [DisableInEditMode, DisableInPlayMode] public string currentState = "NULL";
+
+
+
 
     [HideInEditMode, HideInPlayMode] public Boss2CentralController controller;
     [HideInEditMode, HideInPlayMode] public Animator animator;
@@ -24,75 +23,140 @@ public class Boss2HeadStateMachine : StateMachine, IDamagable
     [HideInEditMode, HideInPlayMode] public int individualDamageCounter;
     [HideInEditMode, HideInPlayMode] public Timer.Loop attackTimer;
 
+    public const string idleState = "Idle";
+    public const string attack1State = "Attack1";
+    public const string attack2State = "Attack2";
+    public const string attack3State = "Attack3";
+    public const string knockedState = "KnockedOut";
+    public const string defendState = "Defend";
+
 
     public bool Damage(Attack attack)
     {
 
         if(headID == FinalBossHead.Pecky)
         {
-            if (knockedState) return false;
+            if (currentState == knockedState) return false;
             if (attack.HasTag("ThrownRock"))
             {
-                knockedState.TransitionTo();
+                SetKnockedState(true);
                 damageTaken++;
             }
         }
         else if(headID == FinalBossHead.Slasher)
         {
-            if (knockedState) return false;
+            if (currentState == knockedState) return false;
             if (individualDamageCounter < 5)
             {
                 individualDamageCounter++;
                 return true;
             }
-            else if (attack.HasTag("Wham"))
+            else if (attack.HasTag(Attack.Tag.Wham))
             {
                 damageTaken++;
-                knockedState.TransitionTo();
+                SetKnockedState(true);
                 individualDamageCounter = 0;
                 return true;
             }
         }
         else if(headID == FinalBossHead.Stumpy)
         {
-            if(attack2State && attack.HasTag("Egg"))
+            if(currentState == "Attack2" && attack.HasTag(Attack.Tag.Egg))
             {
                 individualDamageCounter++;
                 if(individualDamageCounter >= 10)
                 {
-                    knockedState.TransitionTo();
+                    //DO STUN?
                 }
                 return true;
+            }
+            if(currentState == "Vulnerable")
+            {
+                if (individualDamageCounter < 5)
+                {
+                    animator.Play("DamagePause");
+                    individualDamageCounter++;
+                    return true;
+                }
+                else if (attack.HasTag(Attack.Tag.Wham))
+                {
+                    damageTaken++;
+                    controller.Damage(new Attack(1, "FromPlayer", "OnWeakSpot"));
+                    individualDamageCounter = 0;
+                    controller.VulnerableReturn();
+                    return true;
+                }
             }
         }
 
         return false;
     }
 
-    protected override void Initialize()
+    private void Awake()
     {
         animator = GetComponent<Animator>();
         controller = GetComponentInParent<Boss2CentralController>();
         attackTimer.rate = Random.Range(attackTimeLower, attackTimeHigher);
     }
-
-    protected override void FixedUpdate()
+    protected void FixedUpdate()
     {
-        base.FixedUpdate();
-        if (idleState)
+        if (currentState == "Idle")
         {
             attackTimer.Tick(() =>
             {
                 attackTimer.rate = Random.Range(attackTimeLower, attackTimeHigher);
 
-                (damageTaken < damageUntilNewAttack || Random.Range(0, 2) == 1
-                    ? attack1State 
-                    : attack2State)
-                    .TransitionTo();
-
+                DoAttack(damageTaken < damageUntilNewAttack || Random.Range(0, 2) == 1 ? 1 : 2);
             });
         }
     }
+
+
+    #region Controls (Animator Centric)
+
+    public void SetState(string name) => currentState = name;
+    public void HasReturnedToIdle()
+    {
+        currentState = idleState;
+        individualDamageCounter = 0;
+    }
+
+    public void DoAttack(int number)
+    {
+        animator.SetTrigger($"Attack{number}");
+        currentState = $"Attack{number}";
+    }
+
+    public void SetKnockedState(bool knocked)
+    {
+        if (knocked)
+        {
+            animator.CrossFade($"Knocked_{headID}", 0.2f);
+            currentState = "KnockedOut";
+            controller.CheckIfBothKnocked();
+        }
+        else
+        {
+            animator.SetTrigger("Next");
+        }
+    }
+
+    public void SetNext() => animator.SetTrigger("Next");
+
+    public void GoToIdle()
+    {
+        currentState = idleState;
+        individualDamageCounter = 0;
+        animator.CrossFade($"Idle_{headID}", .2f);
+    }
+
+    #endregion
+
+
+    [Button]
+    public void TestKnockOut() => SetKnockedState(true);
+
+
 }
 
 public enum FinalBossHead {Pecky, Slasher, Stumpy}
