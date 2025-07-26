724a492fa375b096dd884ed20969d91aff898e0b
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-07-26 00:09:08 -0400

    Refactor OnGUI CreateStaticMeshWindow() into local methods to make the Rethink easier

--- Assets/Tooling/AssetImportPipeline/Editor/EditorUI/StaticMeshWindow.cs
+++ Assets/Tooling/AssetImportPipeline/Editor/EditorUI/StaticMeshWindow.cs
@@ -36,84 +36,111 @@ namespace AssetImportPipeline
 
         private void CreateStaticMeshWindow()
         {
-            // Which folder should this asset go it?
-            GUILayout.Space(spaceSize);
-            GUILayout.Label("Category");
-            GUILayout.Label(staticMesh.assetCategory);
-            if (GUILayout.Button("Browse..."))
+            SetCategoryUI();
+            SetAssetNameUI();
+            SetModelUI();
+            
+            SetPbrTexturesUI();
+            SetMaterialActionUI();
+
+            FinishImportUI();
+
+            void SetCategoryUI()
             {
-                string path = EditorUtility.OpenFolderPanel("Select File", "Assets/Art", "");
-                if (!string.IsNullOrEmpty(path))
+                // Which folder should this asset go in?
+                GUILayout.Space(spaceSize);
+                GUILayout.Label("Category");
+                GUILayout.Label(staticMesh.assetCategory);
+                if (GUILayout.Button("Browse..."))
                 {
-                    staticMesh.assetCategory = path;
+                    string path = EditorUtility.OpenFolderPanel("Select File", "Assets/Art", "");
+                    if (!string.IsNullOrEmpty(path))
+                    {
+                        staticMesh.assetCategory = path;
+                    }
+                }
+                GUILayout.Space(spaceSize);
+            }
+
+            void SetAssetNameUI()
+            {
+                // What is the asset's name?
+                GUILayout.Label("Asset name");
+                GUI.SetNextControlName("MyTextField");
+                staticMesh.assetName = GUILayout.TextField(staticMesh.assetName);
+                GUILayout.Space(spaceSize);
+            }
+
+            void SetModelUI()
+            {
+                // Model path
+                staticMesh.model = CreateImportButton("Model Path (must be .fbx!)", staticMesh.model, "fbx") as Model;
+                GUILayout.Space(spaceSize);
+            }
+
+            void SetPbrTexturesUI()
+            {
+                // Handle textures
+                if (FbxContainsEmbeddedTextures(staticMesh.model))
+                {
+                    // ???
+                }
+                else
+                {
+                    staticMesh.materialTypeIndex = EditorGUILayout.Popup("Material Type", staticMesh.materialTypeIndex, new[] { "PBR", "Something else" });
+                    staticMesh.transparent = GUILayout.Toggle(staticMesh.transparent, "Transparent");
+
+                    if (staticMesh.materialTypeIndex == 0)
+                    {
+                        // PBR Texture paths
+                        staticMesh.PbrTextures.DiffuseMap = CreateImportButton("Diffuse path", staticMesh.PbrTextures.DiffuseMap, "png") as Texture;
+                        staticMesh.PbrTextures.RoughnessMap = CreateImportButton("Roughness path", staticMesh.PbrTextures.RoughnessMap, "png") as Texture;
+                        staticMesh.PbrTextures.NormalMap = CreateImportButton("Normal path", staticMesh.PbrTextures.NormalMap, "png") as Texture;
+                        staticMesh.PbrTextures.HeightMap = CreateImportButton("Height path", staticMesh.PbrTextures.HeightMap, "png") as Texture;
+                        staticMesh.PbrTextures.EmissiveMap = CreateImportButton("Emissive path", staticMesh.PbrTextures.EmissiveMap, "png") as Texture;
+                        if (staticMesh.transparent) staticMesh.PbrTextures.AlphaMap = CreateImportButton("Alpha path", staticMesh.PbrTextures.AlphaMap, "png") as Texture;
+                    }
+                    else GUILayout.Label("Sorry, this isn't a real option. Choice was an illusion all along.");
+
+                    GUILayout.Space(20);
                 }
             }
-            GUILayout.Space(spaceSize);
 
-            // What is the asset's name?
-            GUILayout.Label("Asset name");
-            GUI.SetNextControlName("MyTextField");
-            staticMesh.assetName = GUILayout.TextField(staticMesh.assetName);
-            GUILayout.Space(spaceSize);
-
-            // Model path
-            staticMesh.model = CreateImportButton("Model Path (must be .fbx!)", staticMesh.model, "fbx") as Model;
-            GUILayout.Space(spaceSize);
-
-            // Handle textures
-            if (FbxContainsEmbeddedTextures(staticMesh.model))
+            void SetMaterialActionUI()
             {
-                // ???
-            }
-            else
-            {
-                staticMesh.materialTypeIndex = EditorGUILayout.Popup("Material Type", staticMesh.materialTypeIndex, new[] { "PBR", "Something else" });
-                staticMesh.transparent = GUILayout.Toggle(staticMesh.transparent, "Transparent");
-
-                if (staticMesh.materialTypeIndex == 0)
+                // Handle material
+                staticMesh.shouldCreateNewMaterial = GUILayout.Toggle(staticMesh.shouldCreateNewMaterial, "Create new material");
+                if (!staticMesh.shouldCreateNewMaterial)
                 {
-                    // PBR Texture paths
-                    staticMesh.PbrTextures.DiffuseMap = CreateImportButton("Diffuse path", staticMesh.PbrTextures.DiffuseMap, "png") as Texture;
-                    staticMesh.PbrTextures.RoughnessMap = CreateImportButton("Roughness path", staticMesh.PbrTextures.RoughnessMap, "png") as Texture;
-                    staticMesh.PbrTextures.NormalMap = CreateImportButton("Normal path", staticMesh.PbrTextures.NormalMap, "png") as Texture;
-                    staticMesh.PbrTextures.HeightMap = CreateImportButton("Height path", staticMesh.PbrTextures.HeightMap, "png") as Texture;
-                    staticMesh.PbrTextures.EmissiveMap = CreateImportButton("Emissive path", staticMesh.PbrTextures.EmissiveMap, "png") as Texture;
-                    if (staticMesh.transparent) staticMesh.PbrTextures.AlphaMap = CreateImportButton("Alpha path", staticMesh.PbrTextures.AlphaMap, "png") as Texture;
+                    GUILayout.Label("Sorry, haven't implemented a way to reuse materials yet :(");
                 }
-                else GUILayout.Label("Sorry, this isn't a real option. Choice was an illusion all along.");
-
                 GUILayout.Space(20);
             }
 
-            // Handle material
-            staticMesh.shouldCreateNewMaterial = GUILayout.Toggle(staticMesh.shouldCreateNewMaterial, "Create new material");
-            if (!staticMesh.shouldCreateNewMaterial)
+            void FinishImportUI()
             {
-                GUILayout.Label("Sorry, haven't implemented a way to reuse materials yet :(");
-            }
-            GUILayout.Space(20);
-
-            // Finalize import dialog
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("");
-            if (GUILayout.Button("Confirm final import", GUILayout.Width(200)))
-            {
-                bool confirm = EditorUtility.DisplayDialog(
-                    "Confirm Import",
-                    "Good work! All strictly necessary information has been entered. :)\n\nAre you ready to import this asset, or would you like to make more adjustments?",
-                    "I'm ready",
-                    "Go Back"
-                );
-
-                if (confirm)
+                // Finalize import dialog
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("");
+                if (GUILayout.Button("Confirm final import", GUILayout.Width(200)))
                 {
-                    StaticMeshImporter staticMeshImporter = new StaticMeshImporter();
-                    staticMeshImporter.ImportStaticMesh(staticMesh);
-                    Close();
+                    bool confirm = EditorUtility.DisplayDialog(
+                        "Confirm Import",
+                        "Good work! All strictly necessary information has been entered. :)\n\nAre you ready to import this asset, or would you like to make more adjustments?",
+                        "I'm ready",
+                        "Go Back"
+                    );
+
+                    if (confirm)
+                    {
+                        StaticMeshImporter staticMeshImporter = new StaticMeshImporter();
+                        staticMeshImporter.ImportStaticMesh(staticMesh);
+                        Close();
+                    }
                 }
+                GUILayout.Label("");
+                GUILayout.EndHorizontal();
             }
-            GUILayout.Label("");
-            GUILayout.EndHorizontal();
         }
 
         AssetBase CreateImportButton(string header, AssetBase asset, string forceType = "")
