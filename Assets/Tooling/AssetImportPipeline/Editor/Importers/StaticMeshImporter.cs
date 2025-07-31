68f560a826c780f51f454d3acaf283c708de4d52
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-07-31 12:34:26 -0400

    Some progress on material import (a lot more to do)

--- Assets/Tooling/AssetImportPipeline/Editor/Importers/StaticMeshImporter.cs
+++ Assets/Tooling/AssetImportPipeline/Editor/Importers/StaticMeshImporter.cs
@@ -25,50 +25,48 @@ namespace AssetImportPipeline
             foreach (Material i in staticMesh.materials)
             {
                 // Create material asset, ensure it's linked to the fbx properly.
+                UnityEngine.Material materialAsset;
 
-                // Create textures, link them to the material properly.
                 switch (i.shader)
                 {
                     case Material.Shaders.CelShaderLit:
+                        materialAsset = new UnityEngine.Material(Shader.Find("Shader Graphs/CelShaderLit"));
                         break;
                     case Material.Shaders.UniversalRenderPipelineLit:
+                        materialAsset = new UnityEngine.Material(Shader.Find("Universal Render Pipeline/Lit"));
+                        // The following lines are from a deprecated method, but may be useful for the real implementation:
+                            // string materialFileName = newSrcFolder + "/" + new Material().GetPrefix() + staticMesh.assetName + ".mat";
+                            // AssetDatabase.CreateAsset(materialAsset, materialFileName);
+                            // AssetDatabase.SaveAssets();
+                            // AssetDatabase.Refresh();
+
+                        // Create textures
+                        CopyAssetIntoProject(staticMesh, i.urplSettings.DiffuseMap, newSrcFolder);
+                        CopyAssetIntoProject(staticMesh, i.urplSettings.RoughnessMap, newSrcFolder);
+
+                        // make sure normal map is set to "normal"
+                        CopyAssetIntoProject(staticMesh, i.urplSettings.NormalMap, newSrcFolder);
+                        if (i.urplSettings.NormalMap.AssetExists())
+                        {
+                            TextureImporter normalMapImporter = AssetImporter.GetAtPath(i.urplSettings.NormalMap.destinationPath) as TextureImporter;
+                            normalMapImporter.textureType = TextureImporterType.NormalMap;
+                            normalMapImporter.SaveAndReimport(); //not sure if this is the right method (the "reimport" part specifically)
+                        }
+
+                        CopyAssetIntoProject(staticMesh, i.urplSettings.HeightMap, newSrcFolder);
+                        CopyAssetIntoProject(staticMesh, i.urplSettings.EmissiveMap, newSrcFolder);
+
+                        // link them to the material properly.
+
                         break;
                 }
             }
 
-
-
-
-
-            // if (staticMesh.materialTypeIndex == 0) // magic numbers yay
-            // {
-            //     CopyAssetIntoProject(staticMesh, staticMesh.PbrTextures.DiffuseMap, newSrcFolder);
-
-            //     CopyAssetIntoProject(staticMesh, staticMesh.PbrTextures.RoughnessMap, newSrcFolder);
-
-            //     // make sure normal map is set to "normal"
-            //     CopyAssetIntoProject(staticMesh, staticMesh.PbrTextures.NormalMap, newSrcFolder);
-            //     if (staticMesh.PbrTextures.NormalMap.AssetExists())
-            //     // if (!string.IsNullOrEmpty(AssetDatabase.AssetPathToGUID(staticMesh.PbrTextures.NormalMap.destinationPath)))
-            //     {
-            //         TextureImporter normalMapImporter = AssetImporter.GetAtPath(staticMesh.PbrTextures.NormalMap.destinationPath) as TextureImporter;
-            //         normalMapImporter.textureType = TextureImporterType.NormalMap;
-            //         normalMapImporter.SaveAndReimport();
-            //     }
-
-
-            //     CopyAssetIntoProject(staticMesh, staticMesh.PbrTextures.HeightMap, newSrcFolder);
-            //     CopyAssetIntoProject(staticMesh, staticMesh.PbrTextures.EmissiveMap, newSrcFolder);
-            // }
-
-            // create material (if it's blank? i think? or maybe the textures shouldn't appear if it's not... also the "update" functionality)
-            // CreateMaterialAssetInProject(staticMesh);
-
-            // apply textures to material
-
             // ??? create the prefab? Profit?
             // possibly something with collision?? if that's not automated???????
+            CreateJson();
         }
+
         void CopyAssetIntoProject(StaticMesh staticMesh, AssetBase asset, string destinationPath)
         {
             if (asset.sourcePath == "No filepath set!") return;
@@ -82,24 +80,10 @@ namespace AssetImportPipeline
             AssetDatabase.ImportAsset(asset.destinationPath);
             Debug.Log("Imported: " + asset.destinationPath);
         }
-
-        void CreateMaterialAssetInProject(StaticMesh staticMesh)
+        
+        void CreateJson()
         {
-            UnityEngine.Material materialAsset = new UnityEngine.Material(Shader.Find("Universal Render Pipeline/Lit"));
-
-            // AssignTextureToMaterial(staticMesh.PbrTextures.DiffuseMap, "_BaseMap");
-            // AssignTextureToMaterial(staticMesh.PbrTextures.MetalnessMap, "")
-
-            string materialFileName = newSrcFolder + "/" + new Material().GetPrefix() + staticMesh.assetName + ".mat";
-            AssetDatabase.CreateAsset(materialAsset, materialFileName);
-            AssetDatabase.SaveAssets();
-            AssetDatabase.Refresh();
-
-            // void AssignTextureToMaterial(Texture texture, string shaderTextureName)
-            // {
-            //     if (texture.AssetExists())
-            //         materialAsset.SetTexture(shaderTextureName, AssetDatabase.LoadAssetAtPath<Texture2D>(texture.destinationPath));
-            // }
+            // Create file in /src that helps serialize the Asset Data Structure for future tooling.
         }
     }
 }
