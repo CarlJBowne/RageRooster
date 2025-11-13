def7873854c30fe83c0d109e2c13f76e62ad4ee1
CBowneUCF <carljbowne@gmail.com>
2025-11-12 20:26:33 -0500

    Initial Script Addition

---
+++ Assets/Scripts/Systems/TargetingSystem/TargetingManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TargetingManager : MonoBehaviour
+{
+
+    public static List<Target> ALLTARGETS = new();
+
+    private static Vector3 FocusPosition;
+    private static Vector3 FocusDirection;
+
+    public static void SetFocus(Vector3 position, Vector3 direction)
+    {
+        FocusPosition = position;
+        FocusDirection = direction;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+public class Target : MonoBehaviour
+{
+
+}
