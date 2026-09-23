3114d781d1ac1a30de64c58de75dde463a2b8392
StarLightShadows <156275809+CarlJBowne@users.noreply.github.com>
2026-09-23 18:19:52 -0400

    Attack Tag fix 1

--- Assets/Scripts/Core/DamageSystem/Attack.cs
+++ Assets/Scripts/Core/DamageSystem/Attack.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Serialization;
 using System.Collections.ObjectModel;
+using SLS.EditorUtilities.Editor;
 
 #if UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine.UIElements;
-using SLS.EditorUtilities.Editor;
 #endif
 
 [System.Serializable]
@@ -21,12 +21,20 @@ public partial class Attack
     public Vector3 velocity = Vector3.zero;
     public TagSet tags = new();
 
+    public Attack() { }
     public Attack(int damage, Vector3 velocity = default, TagSet tags = default)
     {
         this.amount = damage;
         this.velocity = velocity;
         this.tags = tags;
     }
+    public Attack(Attack source)
+    {
+        this.amount = source.amount;
+        this.velocity = source.velocity;
+        this.tags = new(source.tags);
+    }
+    public Attack Clone() => new(this);
 
     public float x => velocity.x;
     public float y => velocity.y;
@@ -51,30 +59,45 @@ public partial class Attack
 
         public override VisualElement CreatePropertyGUI(SerializedProperty property)
         {
-            VisualElement root = new VisualElement();
-
-            displayNameField = new(property.FindPropertyRelative(nameof(_displayName)), "Attack Name");
+            displayNameField = new(property.FindPropertyRelative(nameof(_displayName)));
             amountField = new(property.FindPropertyRelative(nameof(amount)), string.Empty);
             velocityField = new(property.FindPropertyRelative(nameof(velocity)));
             tagsField = new(property.FindPropertyRelative(nameof(tags)));
 
-            CustomFoldout foldout = new();
+            Foldout foldout = new();
+            foldout.Bind(property.serializedObject);
+            foldout.Clear();
+            foldout.style.marginTop = 0;
+            foldout.style.marginBottom = 0;
             foldout.value = false;
-            root.Add(foldout);
 
-            foldout.headerSide.Add(amountField);
-            foldout.contentContainer.Add(displayNameField);
             foldout.contentContainer.Add(velocityField);
             foldout.contentContainer.Add(tagsField);
 
-            displayNameField.RegisterValueChangeCallback(DisplayNameChanged);
-            void DisplayNameChanged(SerializedPropertyChangeEvent ev) => foldout.text = !string.IsNullOrEmpty(ev.changedProperty.stringValue) 
-                    ? ev.changedProperty.stringValue 
-                    : property.displayName;
+            foldout.DelayedBuild(() =>
+            {
+                Label label = foldout.Q<Label>(className: Foldout.textUssClassName);
+                //VisualElement header = label.parent;
 
-            root.Bind(property.serializedObject);
+                //if (label.text.StartsWith("Element "))
+                //    label.text = label.text.Replace("Element ", "Attack ");
+                //label.ShrinkToTextWidth();
+                //label.parent.style.flexDirection = FlexDirection.Row;
 
-            return root; 
+                //header.Add(displayNameField);
+                //header.Add(amountField);
+
+                amountField.style.maxWidth = Length.Percent(.15f);
+
+                //header.Add(amountField);
+            });
+
+            //displayNameField.RegisterValueChangeCallback(DisplayNameChanged);
+            //void DisplayNameChanged(SerializedPropertyChangeEvent ev) => foldout.text = !string.IsNullOrEmpty(ev.changedProperty.stringValue) 
+            //        ? ev.changedProperty.stringValue 
+            //        : property.displayName;
+
+            return foldout; 
         }
     }
 #endif
